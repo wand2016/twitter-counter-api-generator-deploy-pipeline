ce2de76f43e275707dfda5c549e641084f2f3867
Daiki Horiyama <d19921207@gmail.com>
2020-10-01 00:05:50 +0900

    変数

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "github_owner" {
+  default = "wand2016"
+}
+
+variable "github_repo" {
+  default = "twitter-counter-api-generator"
+}
+
+variable "github_token" {
+  description = "PAT (Personal Access Token)"
+  default     = "????"
+}
