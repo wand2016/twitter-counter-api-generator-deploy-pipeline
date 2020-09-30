a3343e0a1f7151a7e0c138090e82a4f3f579f1c2
Daiki Horiyama <d19921207@gmail.com>
2020-10-01 02:43:39 +0900

    hotfix: ssm String -> SecureString

--- ssm.tf
+++ ssm.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "github_personal_access_token" {
   name        = "github-personal-access-token"
   description = "github-personal-access-token"
-  type        = "String"
+  type        = "SecureString"
   value       = var.github_token
 }
