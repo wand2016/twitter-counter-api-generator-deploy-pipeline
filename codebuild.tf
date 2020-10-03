93628ebfa429440d08542d940f4e6ba326fb1c3c
Daiki Horiyama <d19921207@gmail.com>
2020-10-04 04:23:45 +0900

    fixed #18 ビルドキャッシュ

--- codebuild.tf
+++ codebuild.tf
@@ -1,13 +1,15 @@
 module "codebuild_staging" {
-  source           = "./modules/codebuild"
-  stage            = "staging"
-  bucket_name      = "staging-twitter-counter-api"
-  service_role_arn = aws_iam_role.codebuild_service_role.arn
+  source            = "./modules/codebuild"
+  stage             = "staging"
+  bucket_name       = "staging-twitter-counter-api"
+  cache_bucket_name = aws_s3_bucket.build_cache.bucket
+  service_role_arn  = aws_iam_role.codebuild_service_role.arn
 }
 
 module "codebuild_production" {
-  source           = "./modules/codebuild"
-  stage            = "production"
-  bucket_name      = "twitter-counter-api"
-  service_role_arn = aws_iam_role.codebuild_service_role.arn
+  source            = "./modules/codebuild"
+  stage             = "production"
+  bucket_name       = "twitter-counter-api"
+  cache_bucket_name = aws_s3_bucket.build_cache.bucket
+  service_role_arn  = aws_iam_role.codebuild_service_role.arn
 }
--- s3.tf
+++ s3.tf
@@ -1,5 +1,11 @@
 resource "aws_s3_bucket" "pipeline" {
   bucket        = "twitter-counter-api-deploy-pipeline-artifacts"
   acl           = "private"
-  force_destroy = false
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "build_cache" {
+  bucket        = "twitter-counter-api-deploy-pipeline-caches"
+  acl           = "private"
+  force_destroy = true
 }
