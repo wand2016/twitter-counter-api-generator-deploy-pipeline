7d0cad59b128c177b15099043a2ee651380e75a3
Daiki Horiyama <d19921207@gmail.com>
2020-10-04 02:24:04 +0900

    バケット名注入漏れ

--- codebuild.tf
+++ codebuild.tf
@@ -1,11 +1,13 @@
 module "codebuild_staging" {
   source           = "./modules/codebuild"
   stage            = "staging"
+  bucket_name      = "staging-twitter-counter-api"
   service_role_arn = aws_iam_role.codebuild_service_role.arn
 }
 
 module "codebuild_production" {
   source           = "./modules/codebuild"
   stage            = "production"
+  bucket_name      = "twitter-counter-api"
   service_role_arn = aws_iam_role.codebuild_service_role.arn
 }
